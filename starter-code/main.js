73e80794f159ed90b688ecb7549c63c77cffd96f
Gi <gise11e@me.com>
2016-09-16 17:17:54 +0100

    Added alerts for guesses

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,11 @@
 var cardOne = "queen";
-var cardtwo = "queen";
+var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo || cardThree === cardFour) {
+	window.alert ("You found a match");
+}
+else {
+	window.alert ("Sorry. Try again!");
+}
