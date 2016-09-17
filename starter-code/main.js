213c528f47064ff65943f33af66c84b47174afe6
Gi <gise11e@me.com>
2016-09-17 15:28:28 +0100

    Created board and cards set up in js

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,26 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo || cardThree === cardFour) {
-	window.alert ("You found a match");
+//if (cardOne === cardTwo || cardThree === cardFour) {
+//	window.alert ("You found a match");
+//}
+//else {
+//	window.alert ("Sorry. Try again!");
+//}
+
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function() {
+
+	for (var i = 0; i < 4; i++) {
+
+		// Create the div for the card
+		var currentCard = document.createElement('div');	
+		currentCard.className = "card";
+
+		// Add the div to the gameboard
+		gameBoard.appendChild(currentCard);
+	}
 }
-else {
-	window.alert ("Sorry. Try again!");
-}
+
+createBoard();
